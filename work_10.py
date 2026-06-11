20a32ecb3b0b44eb9fe8347f503608e17e651dd7
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:14:39 +0300

    Add files via upload

---
+++ work_10.py
@@ -0,0 +1,63 @@
+#if 25
+# x=float(input("x="))
+# if x<-2 or x>2:
+#     print(2*x)
+# else:
+#     print(-3*x)
+#if 26
+# x=float(input("x="))
+# if x<=0:
+#     print(-x)
+# elif 0<x<2:
+#     print(x**2)
+# elif x>=2:
+#     print(4)
+#if 27
+# x=float(input("x="))
+# if x<0:
+#     print(0)
+# elif 0<=x<1 or 2<=x<3 or x%2==0<=x<x%2==1:
+#     print(1)
+# elif 1<=x<2 or x%2==1<=x<x%2==0:
+#     print(-1)
+# else:
+#     print("Ajoyib")
+#if 28
+# a=int(input("a="))
+# if a%4==0 and a%400==0:
+#     print("Kabisa",366)
+# else:
+#     print("Kabisa emas", 365)
+#if 29
+# a=float(input("a="))
+# if a>0 and a%2==1:
+#     print("Musbat toq son")
+# elif a<0 and a%2==0:
+#     print("Manfiy juft son")
+# elif a=0:
+#     print("Son nolga teng")
+# else:
+#     print("Farhodbek")
+#if 30
+# x=int(input("x="))
+# if 0<x<=9 and x%2==0:
+#     print("juft bir honali son")
+# elif 0<x<=9 and x%2==1:
+#     print("toq bir honali son")
+# if 9<x<=99 and x%2==0:
+#     print("ikki xonali juft son")
+# elif 9<x<=99 and x%2==1:
+#     print("ikki xonali toq son")
+# if 100<=x<=999 and x%2==1:
+#     print("Uch xonali toq son")
+# elif 100<=x<=999 and x%2==0:
+#     print("uch xonali juft son")
+#Boolean 1
+# A=int(input())
+# t=A>0
+# print(t)
+#Boolean 2
+# a=int(input())
+# b=a%2==1
+# c=a>0
+# print(b)
